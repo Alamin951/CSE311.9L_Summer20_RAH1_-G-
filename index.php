27ebefafe1f622814e2dd07a3149e9aa4a8a135d
Alamin951 <alamin.hossain7@northsouth.edu>
2020-09-23 12:55:10 +0600

    umdated

--- index.php
+++ index.php
@@ -3,11 +3,45 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title> HOTEL MANAGEMENT SYSTEM </title>
+	<title> AAS HOTEL </title>
+
 </head>
 <body>
+	<div class="banner-top">
+			
+			<div class="contact-bnr-w3-agile">
+				<ul>
+					<li><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:info@example.com">INFO@AAS.COM</a></li>
+					<li><i class="fa fa-phone" aria-hidden="true"></i>+880176128198</li>	
+					
+				</ul>
+			</div>
+		</div>
+	<div class="w3_navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header navbar-left">
+					
+					<h1><a class="navbar-brand" href="index.php">AAS <span>HOTEL</span><p class="logo_w3l_agile_caption">Your Dreamy Resort</p></a></h1>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+					<nav class="menu menu--iris">
+						<ul class="nav navbar-nav menu__list">
+							<li class="menu__item menu__item--current"><a href="" class="menu__link">Home</a></li>
+							<li class="menu__item"><a href="#about" class="menu__link scroll">About</a></li>
+							<li class="menu__item"><a href="#gallery" class="menu__link scroll">Gallery</a></li>
+							<li class="menu__item"><a href="#rooms" class="menu__link scroll">Rooms</a></li>
+							<li class="menu__item"><a href="#contact" class="menu__link scroll">Contact Us</a></li>
+						</ul>
+					</nav>
+				</div>
+			</nav>
+
+		</div>
+	</div>
 	<div class="container">
-		This is hotel webpage.
+		<h1>This is hotel webpage.</h1>
 	</div>
 
 </body>
